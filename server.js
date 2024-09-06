6c2843ce9f5891903b6379b7a4288cebeb9cbf7e
Yashwanth <Yashwanth@tetraflow.tech>
2024-09-06 23:33:50 +0530

    server update

--- server.js
+++ server.js
@@ -28,5 +28,9 @@ app.post('/validate-token', (req, res) => {
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 
+app.use('/',(req,res) => {
+  res.send("server is running");
+})
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
